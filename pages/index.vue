49037a0ead227fd4a1828c75e037968c81063c12
Gabriel-Gomes-Meira <bielzinhomeira@gmail.com>
2022-06-02 12:16:51 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+ <template>
+    
+    <v-container grid-list-xs>
+        <v-row justify="center">
+            <v-col cols="6">
+                <v-sheet class="white rounded"
+                style="padding:1px"
+                >
+                    <v-card elevation="0" 
+                    class="rounded">                            
+                        <v-card-media src="src" height="200px">
+                        </v-card-media>
+                        <v-card-title primary-title>
+                            <div>
+                                <h3 class="headline mb-0">headline</h3>
+                                <div>description</div>
+                            </div>
+                        </v-card-title>
+                        <v-card-actions>
+                            
+                        </v-card-actions>
+                    </v-card>        
+                </v-sheet>
+            </v-col>
+        </v-row>
+        
+    </v-container>
+       
+ </template>
+ 
+ <script>
+ export default {
+ 
+ }
+ </script>
+ 
+ <style>
+ 
+ </style>
